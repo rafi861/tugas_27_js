b2f06b002b5739b13e200e453dc37db400f56d2d
rafi861 <muhammadrafi2545@gmail.com>
2023-03-11 07:30:09 +0700

    tugas 27 js

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+function tugas() {
+  var a = 'no ';
+  var b = 'rumah ';
+  var c = 'saya ';
+  var d = '30 ';
+  var e = 'a ' ;
+  var f = 'jalan pangeran tirtayasa no ';
+  var g = 70;
+  var kalimat = a.concat(b).concat(c).concat(d).concat(e).concat(f).concat(g);
+  console.log(kalimat);
+
+}
+tugas()
